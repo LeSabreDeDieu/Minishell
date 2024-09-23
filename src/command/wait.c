4db8674b9820a66d8ee30df173998a795330a9e0
SenseiTarzan <gabrielcaptari0@gmail.com>
2024-09-23 12:10:23 +0200

    format on norm42

--- src/command/wait.c
+++ src/command/wait.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   wait.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgabsi <sgabsi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: gcaptari <gcaptari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 01:15:30 by gcaptari          #+#    #+#             */
-/*   Updated: 2024/09/18 16:46:58 by sgabsi           ###   ########.fr       */
+/*   Updated: 2024/09/23 12:10:08 by gcaptari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,52 @@ void	kill_all_pid(t_ast *ast)
 	kill_all_pid(ast->right);
 }
 
-void	wait_process(t_minishell *data, t_ast_value *value, bool is_pipeline)
+void handle_signal_interrupt(t_minishell *data)
+{
+	if (g_signal == SIGQUIT)
+		ft_putendl_fd("\r^\\\033[0KQuit (Core dumped)", 1);
+	kill_all_pid(data->ast);
+	g_signal = 0;
+}
+
+void wait_for_single_process(t_minishell *data, t_ast_value *value)
 {
 	int ret;
 	int states;
+
 	ret = 0;
-	if (value->pid != -1)
+	while (ret >= 0)
 	{
-		while (ret >= 0)
+		ret = waitpid(value->pid, &states, WNOHANG);
+		if (g_signal != 0)
 		{
-			ret = waitpid(value->pid, &states, WNOHANG);
-				if(g_signal != 0){
-				if(g_signal == SIGQUIT)
-					ft_putendl_fd("\r^\\\033[0KQuit (Core dumped)", 1);
-				kill_all_pid(data->ast);
-				g_signal = 0;
-				break;
-			}
+			handle_signal_interrupt(data);
+			break;
 		}
-		data->current_status = WEXITSTATUS(states);
 	}
-	if (is_pipeline)
+	data->current_status = WEXITSTATUS(states);
+}
+
+void wait_for_pipeline(t_minishell *data)
+{
+	int ret;
+
+	ret = 0;
+	while (ret >= 0)
 	{
-		ret = 0;
-		while (ret >= 0){
-			ret = wait3(NULL,  WNOHANG, NULL);
-			if(g_signal != 0){
-				if(g_signal == SIGQUIT)
-					ft_putendl_fd("\r^\\\033[0KQuit (Core dumped)", 1);
-				kill_all_pid(data->ast);
-				g_signal = 0;
-				break;
-			}
+		ret = wait3(NULL, WNOHANG, NULL);
+		if (g_signal != 0)
+		{
+			handle_signal_interrupt(data);
+			break;
 		}
-		return;
 	}
+}
+
+void wait_process(t_minishell *data, t_ast_value *value, bool is_pipeline)
+{
+	if (value->pid != -1)
+		wait_for_single_process(data, value);
+	if (is_pipeline)
+		wait_for_pipeline(data);
 }
